cecbfc7e3760df8e393fa8070d9cc991e9bb452f
root <fattisenmalan@outlook.com>
2023-06-15 23:52:39 -0700

    fixed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#description: main function prints using puts
+/*#description: main function prints using puts*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
