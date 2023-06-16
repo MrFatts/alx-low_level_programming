cf429373e51139977f36bd6b6045d2b369a6b166
root <fattisenmalan@outlook.com>
2023-06-15 23:43:52 -0700

    fixed puts and printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#description: main function prints using puts
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
+puts("\n");
 return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#description: main description print using printf
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,");
+printf("\n");
 return (0);
 }
