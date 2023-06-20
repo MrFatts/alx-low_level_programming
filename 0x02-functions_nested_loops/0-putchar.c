1aa219d835fa7b89d07b24957422922b04f7eebc
root <fattisenmalan@outlook.com>
2023-06-20 09:16:16 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <main.h>
+
+/**
+ * main - the program uses a custom header
+ *
+ * Return: main is used instead of stdio
+ */
+
+int main(void)
+{
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
+	return (0);
+
+}
