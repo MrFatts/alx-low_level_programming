3a670cc79de1510237341f4cc200ddcfb3fdb3cb
root <fattisenmalan@outlook.com>
2023-07-05 19:04:01 -0700

    here we go again

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _puts_recursion - recurses over a string
+ *
+ * @s: parameter
+ *
+ * Return: Accepts 0
+ *
+ */
+
+void _puts_recursion(char *s)
+{
+	if (!(*s))
+	{
+		_putchar('\n');
+		return;
+	}
+
+	_putchar(*s);
+	_puts_recursion(s + 1);
+
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,5 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int _putchar(char c);
+void _puts_recursion(char *s);
