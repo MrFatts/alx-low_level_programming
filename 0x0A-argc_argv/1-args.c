714c20a8624e6fcd5555a3d1806f5197041884e1
root <fattisenmalan@outlook.com>
2023-07-08 10:51:42 -0700

    numbers the arguments passed

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - print the number of arguments in program
+ * 
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Accept 0
+ */
+
+int main(int argc, char **argv __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
