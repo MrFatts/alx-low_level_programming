95c8212b911cb2cb8b52a26ae4e7dc7d45627411
root <fattisenmalan@outlook.com>
2023-07-18 15:56:54 -0700

    prints name, age and name of owner from dog.h file which is a struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef __DOG_H__
+#define __DOG_H__
+
+/**
+ * struct dog - defines new data type struct dog
+ *
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner
+ *
+ */
+struct dog {
+
+	char *name;
+	char *owner;
+	float age;
+};
+
+
+
+#endif
