82b26ec006b45db354b64086760d234018f9ad23
root <fattisenmalan@outlook.com>
2023-07-18 17:10:43 -0700

    adds new data of type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,6 +17,13 @@ struct dog
 	char *owner;
 	float age;
 };
+
+/**
+ * dog_t - typedef for struct dog
+ *
+ */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
